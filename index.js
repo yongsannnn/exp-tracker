03428cf83375543fda7316945f8c55ecdd28ab63
yongsannnn <yong_s@live.com>
2021-05-12 05:56:14 +0000

    added route to add expenses

--- index.js
+++ index.js
@@ -5,20 +5,53 @@ require("dotenv").config();
 const MongoUtil = require("./MongoUtil");
 const mongoUrl = process.env.MONGO_URL;
 const ObjectId = require("mongodb").ObjectId;
-let app = express ();
+let app = express();
 app.use(express.json());
 app.use(cors());
 // Setup end
 
 
-async function main(){
+async function main() {
     let db = await MongoUtil.connect(mongoUrl, "expense_tracker")
     console.log("Database connected")
 
     // Get all document
-    app.get("/", async (req,res)=>{
+    app.get("/", async (req, res) => {
         let results = await db.collection("expenses").find().toArray();
         res.send(results)
+    })
+
+    // Get all expenses based on the user id
+    app.post("/expenses", async (req, res) => {
+        try {
+            let results = await db.collection("expenses").find({
+                user_id: req.body.user_id
+            }).toArray()
+            res.send(results)
+        } catch (e) {
+            console.log(e)
+            res.status(500)
+            res.send("Unable to get data.")
+        }
+    })
+
+    // Create expenses 
+    app.post("/expenses/add", async (req, res) => {
+        try {
+            await db.collection("expenses").insertOne({
+                user_id: req.body.user_id,
+                amount: req.body.amount,
+                date: req.body.date,
+                category: req.body.category,
+                memo: req.body.memo
+            })
+            res.status(200)
+            res.send("Expenses added.")
+        } catch (e) {
+            console.log(e)
+            res.status(500)
+            res.send("Unable to add expenses.")
+        }
     })
 }
 
